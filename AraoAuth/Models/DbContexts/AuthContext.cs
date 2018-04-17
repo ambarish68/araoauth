b16f63659be746ef9205d630892d976d331c5ad0
ambarish68 <ambarish.v.rao@gmail.com>
2018-04-17 23:26:55 +0530

    initial oauth implementation

---
+++ AraoAuth/Models/DbContexts/AuthContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AraoAuth.Models.DbContexts
+{
+    public class AuthContext : IdentityDbContext<IdentityUser>
+    {
+        public AuthContext() : base("AuthContext")
+        {
+
+        }
+    }
+}
